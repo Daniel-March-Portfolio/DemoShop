8a73de840f02faf0d1fc176d6d72328eb194d383
Daniel-March <town.kor@gmail.com>
2023-12-30 20:15:37 +0300

    Changed Item 'price' field type

--- Item/models.py
+++ Item/models.py
@@ -11,9 +11,20 @@ class Item(models.Model):
 
     title = models.CharField(max_length=60)
     description = models.TextField(max_length=800)
-    price = models.IntegerField(validators=[MinValueValidator(0, "Price cannot be negative")])
+    price = models.FloatField(validators=[MinValueValidator(0, "Price cannot be negative")])
 
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
 
     def __str__(self):
         return f"{self.title} ({self.category})"
+
+    def save(
+            self, force_insert=False, force_update=False, using=None, update_fields=None
+    ):
+        self.price = round(self.price, 2)
+        super().save(
+            force_insert=force_insert,
+            force_update=force_update,
+            using=using,
+            update_fields=update_fields
+        )
