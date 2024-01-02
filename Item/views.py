e0321f3bd442d1466f85ab4b06d49624c79670e4
Daniel-March <town.kor@gmail.com>
2024-01-02 11:56:39 +0300

    Changed ItemViewSet ModelViewSet to ReadOnlyModelViewSet

--- Item/views.py
+++ Item/views.py
@@ -1,12 +1,12 @@
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import permissions
-from rest_framework.viewsets import ModelViewSet
+from rest_framework.viewsets import ReadOnlyModelViewSet
 
 from Item.models import Item
 from Item.serializers import ItemSerializer
 
 
-class ItemViewSet(ModelViewSet):
+class ItemViewSet(ReadOnlyModelViewSet):
     queryset = Item.objects.all()
     serializer_class = ItemSerializer
 
